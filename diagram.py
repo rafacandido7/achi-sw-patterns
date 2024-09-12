27f0af872868cd6ae5ae97a4e91990683795f559
Rafael Candido <rafacandido0708@gmail.com>
2024-09-12 10:32:47 -0300

    start repo

---
+++ diagram.py
@@ -0,0 +1,5 @@
+from diagrams import Diagram
+from diagrams.onprem.database import PostgreSQL, Neo4J
+
+with Diagram("Route Otimization", filename="route-otimization", show=True, outformat="png"):
+    Neo4J("Graph Database")
