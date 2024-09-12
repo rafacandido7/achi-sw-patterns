73a655dd61679dc6d770f9fbdbc5ffaf19aae411
Rafael Candido <rafacandido0708@gmail.com>
2024-09-12 10:38:13 -0300

    enunciado

--- diagram.py
+++ diagram.py
@@ -1,5 +1,5 @@
 from diagrams import Diagram
-from diagrams.onprem.database import PostgreSQL, Neo4J
+from diagrams.onprem.database import Neo4J
 
 with Diagram("Route Otimization", filename="route-otimization", show=True, outformat="png"):
     Neo4J("Graph Database")
