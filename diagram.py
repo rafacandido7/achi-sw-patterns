dd4736ec3167a3f8bcb6674241b68c075516cbac
Rafael Candido <rafacandido0708@gmail.com>
2024-09-21 13:54:52 -0300

    feat: archi

--- diagram.py
+++
@@ -1,5 +0,0 @@
-from diagrams import Diagram
-from diagrams.onprem.database import Neo4J
-
-with Diagram("Route Otimization", filename="route-otimization", show=True, outformat="png"):
-    Neo4J("Graph Database")
